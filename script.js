b10518ccf1aaeeaea688ce4d1fd4ae51f034163f
amit-raj8 <amitrajj8092@gmail.com>
2024-12-08 23:36:07 +0530

    adding some javascript syntax

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ const messageInput = document.querySelector(".message-input");
 
 messageInput.addEventListener("keydown", () => {
     const userMessage = e.target.value.trim();
+    if(e.key === "Enter" && userMessage){
+        
+    }
 })
