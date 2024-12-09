c9da60764b5a8bf76ced9c8df8101765d584c798
amit-raj8 <amitrajj8092@gmail.com>
2024-12-09 22:47:50 +0530

    working on js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const messageInput = document.querySelector(".message-input");
 messageInput.addEventListener("keydown", () => {
     const userMessage = e.target.value.trim();
     if(e.key === "Enter" && userMessage){
-        
+        handleOutgoingMessage(userMessage);
     }
 })
