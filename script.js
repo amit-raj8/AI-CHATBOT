9d99727991a9fdfe5f971b61adce184028e360c4
amit-raj8 <amitrajj8092@gmail.com>
2024-12-09 23:47:47 +0530

    working on js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const messageInput = document.querySelector(".message-input");
 
+const handleOutgoingMessage = (userMessage) => {
+    const messageContent = ` <div class="message-text">
+              ${userMessage}
+            </div>`}
+            createMessageElement(messageContent, "user-message");
+
 messageInput.addEventListener("keydown", () => {
     const userMessage = e.target.value.trim();
     if(e.key === "Enter" && userMessage){
