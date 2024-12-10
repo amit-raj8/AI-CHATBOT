31629a25ab4a4126b0a8f6a33ccd4f0a753c6311
amit-raj8 <amitrajj8092@gmail.com>
2024-12-10 15:23:12 +0530

    adding some js syntax on js ffile

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
+const chatBody = document.querySelector(".chat-body");
 const messageInput = document.querySelector(".message-input");
+const sendMessageButton = document.querySelector("#send-message");
 
-const handleOutgoingMessage = (userMessage) => {
-    const messageContent = ` <div class="message-text">
-              ${userMessage}
-            </div>`}
-            createMessageElement(messageContent, "user-message");
+const userData = {
+    message: null
+}
 
-messageInput.addEventListener("keydown", () => {
+const createMessageElement = (content, classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
+}
+
+const handleOutgoingMessage = (e) => {
+  e.preventDefault();   
+  userData.message = messageInput.value.trim();
+
+  const messageContent = ` <div class="message-text"></div>`;
+  
+  const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
+  outgoingMessageDiv.querySelector(".message-text").textContent = userData.message;
+  chatBody.appendChild(outgoingMessageDiv);
+
+}
+
+messageInput.addEventListener("keydown", (e) => {
     const userMessage = e.target.value.trim();
     if(e.key === "Enter" && userMessage){
-        handleOutgoingMessage(userMessage);
+        handleOutgoingMessage(e);
     }
-})
+});
+
+sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e))
